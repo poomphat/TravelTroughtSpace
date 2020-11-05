8a89b907f44cd88e8234d98a4b39005f4ef98ed1
poomphat wawongmoon <pumphat@hotmail.co.th>
2020-11-05 17:35:36 +0700

    add

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-//import MyloginPage from './component/login';
+import MyloginPage from './component/login';
 //import Profile from './component/Profile';
 //import MainScreen from './screen/mainScreen';
 import PlanetNavigator from './navigation/SpaceNavigator'
 export default function App() {
   return (
-      <PlanetNavigator></PlanetNavigator>
+      <MyloginPage></MyloginPage>
   );
 }
 const styles = StyleSheet.create({
--- component/login.js
+++ component/login.js
@@ -1,101 +1,165 @@
-import React, {useState} from 'react';
-import { ImageBackground,StyleSheet, Text, View, Animated, Image, Dimensions,TextInput, Button} from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { ImageBackground, StyleSheet, Text, View, Animated, Image, Dimensions, TextInput, Button,Easing } from 'react-native';
 import LoginBox from "./loginBox";
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as firebase from 'firebase';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyD7hO4qUYDmmucGuiEvXAlN2WZQfh4Q5DY",
+    authDomain: "mobileapptroughtthespace.firebaseapp.com",
+    databaseURL: "https://mobileapptroughtthespace.firebaseio.com",
+    projectId: "mobileapptroughtthespace",
+    storageBucket: "mobileapptroughtthespace.appspot.com",
+    messagingSenderId: "580594228095",
+    appId: "1:580594228095:web:fc4cde3c0326b0f482584a",
+    measurementId: "G-W6H3W2SL6P"
+};
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
+const MyloginPage = (props) => {
 
-const MyloginPage = (props) =>{
+    const movebg = useRef(new Animated.Value(0)).current;
+    const translate = movebg.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg','360deg'],
+    });
+    const ani = () => {
+        Animated.loop(
+            Animated.timing(
+            movebg, {
+            toValue: 1,
+            duration: 20000,
+            useNativeDriver: true,
+            easing : Easing.linear,
+        })).start( ()=>{movebg.setValue(0)});
+    }
+
     const [loginState, setLoginState] = useState('login');
+    const [Username, setUsername] = useState('');
+    const [Password, setPassword] = useState('');
     const onpressLogin = (text) => {
         setLoginState(text)
     }
+    const signUpUser = (email, password) => {
+        try {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+        }
+        catch (error) {
+            console.log(error.toString())
+        }
+    }
+    const loginUser = (email, password) => {
+
+        try {
+
+            firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+                console.log('finishlogin')
+
+            })
+        }
+        catch (error) {
+            console.log(error.toString())
+        }
+    }
     const RenderTheBox = (boxType) => {
-        if (boxType === 'login'){
-            return(
+        if (boxType === 'login') {
+            return (
                 <View style={styles.loginBox}>
-                        <Text style={styles.Label}>Login</Text>
-                        <TextInput 
-                            placeholder={'Username'} 
-                            style={styles.TextBox}
-                        />
-                        <TextInput 
-                            placeholder={'Password'} 
-                            style={styles.TextBox}
-                        />
-                        <TouchableOpacity style={styles.Loginbutton}
-                            onPress={() => onpressLogin('login')}
-                        ><Text style={styles.Textinbutton}>Login</Text></TouchableOpacity>
+                    <Text style={styles.Label}>Login</Text>
+                    <TextInput
+                        placeholder={'Username'}
+                        style={styles.TextBox}
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        onChangeText={(email) => setUsername(email)}
+                    />
+                    <TextInput
+                        placeholder={'Password'}
+                        style={styles.TextBox}
+                        secureTextEntry={true}
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        onChangeText={(pass) => setPassword(pass)}
+                    />
+                    <TouchableOpacity style={styles.Loginbutton}
+                        onPress={() => loginUser(Username, Password)}
+                    ><Text style={styles.Textinbutton}>Login</Text></TouchableOpacity>
 
-                        <Text style={{color:'grey', marginTop:5,}}>Don't have an account?</Text>
+                    <Text style={{ color: 'grey', marginTop: 5, }}>Don't have an account?</Text>
 
-                        <TouchableOpacity style={styles.regisbutton}
-                            onPress={() => onpressLogin('register')}
-                        ><Text style={styles.Textinbutton}>Register</Text></TouchableOpacity>
-                    </View>
+                    <TouchableOpacity style={styles.regisbutton}
+                        onPress={() => (onpressLogin('register'))}
+                    ><Text style={styles.Textinbutton}>Register</Text></TouchableOpacity>
+                </View>
             );
         }
-        else if(boxType === 'register'){
-            return(
+        else if (boxType === 'register') {
+            return (
                 <View style={[styles.loginBox, styles.regisBox]}>
-                        <Text style={styles.Label}>Register</Text>
-                        <TextInput 
-                            placeholder={'Username'} 
-                            style={styles.TextBox}
-                        />
-                        <TextInput 
-                            placeholder={'Password'} 
-                            style={styles.TextBox}
-                        />
-                        <TextInput 
-                            placeholder={'Confirm Password'} 
-                            style={styles.TextBox}
-                        />
-                        <TextInput 
-                            placeholder={'Email'} 
-                            style={styles.TextBox}
-                        />
-                        <TouchableOpacity style={styles.regisbutton}
-                            onPress={() => onpressLogin('login')}
-                        ><Text style={styles.Textinbutton}>Register</Text></TouchableOpacity>
+                    <Text style={styles.Label}>Register</Text>
+                    <TextInput
+                        placeholder={'Username'}
+                        style={styles.TextBox}
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        onChangeText={(email) => setUsername(email)}
+                    />
+                    <TextInput
+                        placeholder={'Password'}
+                        style={styles.TextBox}
+                        secureTextEntry={true}
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        onChangeText={(pass) => setPassword(pass)}
+                    />
+                    <TextInput
+                        placeholder={'Confirm Password'}
+                        style={styles.TextBox}
+                    />
+                    <TouchableOpacity style={styles.regisbutton}
+                        onPress={() => signUpUser(Username, Password)}
+                    ><Text style={styles.Textinbutton}>Register</Text></TouchableOpacity>
 
-                        <TouchableOpacity style={styles.cancelbutton}
-                            onPress={() => onpressLogin('login')}
-                        ><Text style={styles.Textinbutton}>Cancel</Text></TouchableOpacity>
-                    </View>
+                    <TouchableOpacity style={styles.cancelbutton}
+                        onPress={() => onpressLogin('login')}
+                    ><Text style={styles.Textinbutton}>Cancel</Text></TouchableOpacity>
+                </View>
             );
         }
-    }        
-    
+    }
+
     return (
         <View style={styles.container}>
-            <ImageBackground source={require('../assets/bg.png')} style={styles.bg} resizeMode="repeat"/>
-            {RenderTheBox(loginState)} 
-        </View>  
+            <Animated.Image source={require('../assets/bg.png')} style={{
+                transform: [{ rotate: translate }], 
+                position: 'absolute',
+                height: '150%',
+            }} resizeMode="repeat" />
+            {RenderTheBox(loginState)}
+            {ani()}
+        </View>
     );
 };
 export default MyloginPage;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: "center",
-        width:Dimensions.get('window').width,
-        height:Dimensions.get('window').height,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
-    bg:{
-        position: 'absolute',
-        width:Dimensions.get('window').width,
-        height:Dimensions.get('window').height,
-    },
-    loginBox:{
+    loginBox: {
         alignItems: 'center',
-        width:Dimensions.get('window').width - 70,
-        height:Dimensions.get('window').height - 425,
-        backgroundColor:'white',
-        borderRadius:10,
-        padding:20,
+        width: Dimensions.get('window').width - 70,
+        height: Dimensions.get('window').height - 425,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 20,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -106,51 +170,51 @@ const styles = StyleSheet.create({
 
         elevation: 23,
     },
-    regisBox:{
-        width:Dimensions.get('window').width - 70,
-        height:Dimensions.get('window').height - 320,
+    regisBox: {
+        width: Dimensions.get('window').width - 70,
+        height: Dimensions.get('window').height - 320,
     },
-    Label:{
-        position:"relative",
-        fontSize:20
+    Label: {
+        position: "relative",
+        fontSize: 20
     },
-    TextBox:{
+    TextBox: {
         padding: 10,
-        margin:10,
-        width:200, 
-        height:50, 
+        margin: 10,
+        width: 200,
+        height: 50,
         borderBottomWidth: StyleSheet.hairlineWidth,
-        borderRadius:7,
+        borderRadius: 7,
     },
-    Loginbutton:{
+    Loginbutton: {
         marginTop: 10,
         borderRadius: 7,
         width: 200,
         backgroundColor: "rgb(0,142,255)",
         height: 40,
         justifyContent: 'center',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    regisbutton:{
+    regisbutton: {
         marginTop: 10,
         borderRadius: 7,
         width: 200,
         backgroundColor: "rgb(0,142,255)",
         height: 40,
         justifyContent: 'center',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    cancelbutton:{
+    cancelbutton: {
         marginTop: 10,
         borderRadius: 7,
         width: 200,
         backgroundColor: "grey",
         height: 40,
         justifyContent: 'center',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    Textinbutton:{
+    Textinbutton: {
         color: 'white',
     }
 
-  });
+});
