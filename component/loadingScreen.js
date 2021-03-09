dc60b4fb296b8da5250d804f371f3561b0b57a89
poomphat wawongmoon <pumphat@hotmail.co.th>
2021-03-09 23:36:50 +0700

    add

--- component/loadingScreen.js
+++ component/loadingScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, } from 'react-native';
 import LottieView from 'lottie-react-native';
-import * as Font from 'expo-font';
 import { useFonts } from 'expo-font';
 import ProgressBar from 'react-native-progress/Bar'
 
